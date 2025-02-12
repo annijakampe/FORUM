100cb3416806173b64bbbd3df03bb7b1eaf52e9f
annijakampe <annija.kampe@gmail.com>
2025-02-12 14:55:04 +0200

    updated

--- includes/new_topic.php
+++ includes/new_topic.php
@@ -3,13 +3,15 @@
 
 <head>
     <title>Create Topic</title>
-    <link rel="stylesheet" href="../assets/AddTopic.css">
+    <link rel="stylesheet" href="../assets/new_topic.css">
 </head>
 
 <body>
-    <h1>Create new Topic</h1>
+    <h1>Create New Topic</h1>
     <form action="../includes/api.php?action=new_topic" method="POST">
-        <input type="text" name="title" required placeholder="Enter topic title"><br>
+        <input type="text" name="title" required placeholder="Enter topic title"
+            onfocus="this.placeholder = ''"
+            onblur="this.placeholder = 'Enter topic title'"><br>
         <button type="submit">Create Topic</button>
         <a href="../public/index.php" class="btn btn-primary">Back to Home</a>
     </form>
